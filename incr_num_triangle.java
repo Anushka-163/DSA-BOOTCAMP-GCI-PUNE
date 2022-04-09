b490e91b7487857755fc8a6fff7ccd9173f5574c
Annu_163! <anushka7923singh@gmail.com>
2022-04-09 16:20:25 +0530

    Create incr_num_triangle.java

---
+++ incr_num_triangle.java
@@ -0,0 +1,19 @@
+# DSA-BOOTCAMP-GCI-PUNE
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+"\t");
+                num++;
+            }
+            System.out.println();
+            
+        }
+    }
+}
